f50d8732bccfa72d3fa3f67a7c48fce566442f5e
FrancescoAceto <acetof02@gmail.com>
2023-03-04 18:54:27 +0100

    FINE ESERCIZIO

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
+
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
@@ -33,4 +34,23 @@ UserButtonEL.addEventListener("click", function() {
     };
     
 });
+
+//Gioco dei dadi.
+let DiceButtonEL = document.getElementById ("DiceThrow");
+let DiceOutputEL = document.getElementById ("DiceThrowOutput")
+
+DiceButtonEL.addEventListener("click", function() {
+    let RandomNumberPlayer = Math.floor(Math.random() * 6 + 1);
+
+    let RandomNumberComputer = Math.floor(Math.random() * 6 + 1);
+
+    if (RandomNumberPlayer > RandomNumberComputer) {
+        DiceOutputEL.innerHTML = `Giocatore: ${RandomNumberPlayer}, Computer: ${RandomNumberComputer}, Hai Vinto.`;
+    
+    } else if(RandomNumberPlayer < RandomNumberComputer) {
+        DiceOutputEL.innerHTML = `Giocatore: ${RandomNumberPlayer}, Computer: ${RandomNumberComputer}, Hai Perso.`;
+    }
+});
+
+
 
